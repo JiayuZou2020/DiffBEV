552a462ca885babd76dc73a6b41328fa44ae114f
JiayuZou2020 <77472466+JiayuZou2020@users.noreply.github.com>
2023-03-15 11:25:34 +0800

    Configurations for datasets

---
+++ configs/_base_/datasets/kittiobject.py
@@ -0,0 +1,55 @@
+# dataset settings
+dataset_type = 'KittiObjectDataset'
+# enter your path of the KITTI 3D Object dataset
+data_root = "YOUR_PATH/kitti_object/"
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=False, with_calib_kittiobject=True, imdecode_backend='pyramid'),
+    dict(type='Resize', img_scale=(1024,1024), resize_gt=False, keep_ratio=False),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'], meta_keys=('filename', 'ori_filename', 'ori_shape',
+                            'img_shape', 'pad_shape', 'scale_factor', 'img_norm_cfg', 'calib')),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=False, with_calib_kittiobject=True, imdecode_backend='pyramid'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024,1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=False, resize_gt=False),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'], meta_keys=('filename', 'ori_filename', 'ori_shape',
+                            'img_shape', 'pad_shape', 'scale_factor', 'flip',
+                            'flip_direction', 'img_norm_cfg', 'gt_semantic_seg', 'calib')),
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='img_dir/train',
+        ann_dir='ann_bev_dir/train',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='img_dir/val',
+        ann_dir='ann_bev_dir/val',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='img_dir/val',
+        ann_dir='ann_bev_dir/val',
+        pipeline=test_pipeline))
