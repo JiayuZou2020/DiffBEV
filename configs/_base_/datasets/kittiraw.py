3916281276c06329e8c48c869cc42e3dc08cd084
JiayuZou2020 <77472466+JiayuZou2020@users.noreply.github.com>
2023-03-15 16:15:00 +0800

    Update the path of KITTI Raw

--- configs/_base_/datasets/kittiraw.py
+++ configs/_base_/datasets/kittiraw.py
@@ -1,7 +1,7 @@
 # dataset settings
 dataset_type = 'KittiRawDataset'
 # enter your path of the KITTI Raw dataset
-data_root = "YOUR_PATH/kitti_raw/"
+data_root = "YOUR_PATH/kitti_processed/kitti_raw/"
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
