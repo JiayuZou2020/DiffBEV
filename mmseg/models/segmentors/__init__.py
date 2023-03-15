6b02063c4bdfaf232416e8d981198029c0e348f8
JiayuZou2020 <77472466+JiayuZou2020@users.noreply.github.com>
2023-03-15 14:32:33 +0800

    Update the init file

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -1,8 +1,6 @@
 from .base import BaseSegmentor
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
-from .bevsegmentor import BEVSegmentor, new_pyva_BEVSegmentor, Depth_new_pyva_BEVSegmentor
 
-__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder',\
-        'BEVSegmentor','new_pyva_BEVSegmentor','Depth_new_pyva_BEVSegmentor',
+__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder',
     ]
